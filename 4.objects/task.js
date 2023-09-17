19ca562e976135e73c4af5025c95a612e89035a5
Taneda Danya <denshkur@yandex.ru>
2023-09-17 11:16:25 +0400

    Задания по обьектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...mark) {
+	if (this.marks != undefined) {
+		this.marks.push(...mark);
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
+	if ((this.marks === undefined) || (this.marks.length === 0)) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, item) => {
+			return acc + item;
+		}) / this.marks.length;
+	}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+	delete this.subject;
+	delete this.marks;
+
+	this.excluded = reason;
 }
